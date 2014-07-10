1208871e42e6f0eb02f4b3775cc1c3ef22a28d5b
Tony Tran <toant713@gmail.com>
2014-07-09 20:30:13 -0700

    Added response and request models

--- KitchenPassWebApp/SpouseChoices/models.py
+++ KitchenPassWebApp/SpouseChoices/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class SpouseRequest(models.Model):
+    question = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.question
+    
+class SpouseResponse(models.Model):
+    answer = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.answer
