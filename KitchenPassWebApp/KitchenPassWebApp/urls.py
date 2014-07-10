e523cb84c30be256475051e00f1c2f2ced08dddf
Tony Tran <toant713@gmail.com>
2014-07-09 21:30:55 -0700

    Added templates folder and templates for models

--- KitchenPassWebApp/KitchenPassWebApp/urls.py
+++ KitchenPassWebApp/KitchenPassWebApp/urls.py
@@ -1,6 +1,7 @@
+from SpouseChoices import views
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<SpouseRequest_id>\d+)/$', views.detail, name='detail'),
+
 )
--- KitchenPassWebApp/SpouseChoices/views.py
+++ KitchenPassWebApp/SpouseChoices/views.py
@@ -1,3 +1,17 @@
+from SpouseChoices.models import SpouseRequest, SpouseResponse
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.template.context import RequestContext
+
 
 # Create your views here.
+# def index(request):
+#     spouseReq = SpouseRequest.objects.order_by('question')[:5]
+    
+def index(request):
+    top_spouse_requests = SpouseRequest.objects.order_by('question')[:5]
+    context = RequestContext(request, {'top_spouse_requests' : top_spouse_requests,})
+    return render(request, 'index.html', context)
+
+def detail(request, SpouseRequest_id):
+    return HttpResponse("DETAIL PAGE: %s" % SpouseRequest_id)
